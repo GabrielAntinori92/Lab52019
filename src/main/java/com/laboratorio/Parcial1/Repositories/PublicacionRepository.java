1b2bb98093c21d5be2b76be5b6658c03a36cdb5f
GabrielAntinori92 <luisgantinori_1992@hotmail.com>
2019-06-24 22:49:20 -0300

    update

--- src/main/java/com/laboratorio/Parcial1/Repositories/PublicacionRepository.java
+++ src/main/java/com/laboratorio/Parcial1/Repositories/PublicacionRepository.java
@@ -7,6 +7,9 @@ import org.springframework.data.jpa.repository.Query;
 
 public interface PublicacionRepository extends JpaRepository<Publicacion, Integer> {
 
-    @Query(value = "select p.titulo as titulo, s.nombre as nombre, count(p.comentario_id) as cantidad from publicacion p join usuario s where p.usuario_id = usuario_id group by p.titulo", nativeQuery = true)
+        @Query(value = "select p.titulo, s.nombre, count(c.id) from publicacion p "
+                + " inner join usuario s on p.usuario_id = s.id"
+                + " inner join comentario c on p.comentario_id = c.id"
+                + " where p.titulo = 'publicacione1'", nativeQuery = true)
     PublicacionDTO findByTitulo(String titulo);
 }
